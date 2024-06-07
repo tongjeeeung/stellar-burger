0913502678ee13a5a9ee5c0245867a50bdc70daa
Tong <tongjeeeung@gmail.com>
2024-06-07 17:12:20 +0300

    fix tests

--- cypress/e2e/constructor/constructor.cy.tsx
+++ cypress/e2e/constructor/constructor.cy.tsx
@@ -1,21 +1,28 @@
 import orderJson from '../../fixtures/order.json';
 
+const testUrl = 'http://localhost:4000';
+const bunSelector = '[data-cy="bun"]';
+const mainSelector = '[data-cy="main"]';
+const sauceSelector = '[data-cy="sauce"]';
+const orderButtonSelector = '[data-cy="order-button"]';
+const modalsSelector = '#modals';
+
 describe('проверяем доступность приложения (модальные окна и ингредиенты)', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
-    cy.visit('http://localhost:4000');
+    cy.visit(testUrl);
   });
 
   it('проверка на перехват ингридиентов и добавления в заказ', () => {
-    cy.get('[data-cy="bun"]').should('have.length.at.least', 1);
-    cy.get('[data-cy="main"],[data-cy="sauce"]').should(
+    cy.get(bunSelector).should('have.length.at.least', 1);
+    cy.get(`${mainSelector}, ${sauceSelector}`).should(
       'have.length.at.least',
       1
     );
 
-    const bun = cy.get('[data-cy="bun"]').find('.common_button');
-    const main = cy.get('[data-cy="main"]').find('.common_button');
-    const sauce = cy.get('[data-cy="sauce"]').find('.common_button');
+    const bun = cy.get(bunSelector).find('.common_button');
+    const main = cy.get(mainSelector).find('.common_button');
+    const sauce = cy.get(sauceSelector).find('.common_button');
 
     bun.click();
     main.click();
@@ -24,37 +31,37 @@ describe('проверяем доступность приложения (мод
 
   describe('модальные окна', () => {
     it('проверка модального окна (открытие)', () => {
-      const main = cy.get('[data-cy="main"]');
+      const main = cy.get(mainSelector);
       main.click();
-      cy.get('#modals').children().should('have.length', 2);
+      cy.get(modalsSelector).children().should('have.length', 2);
     });
 
     it('проверка модального окна (открытие && перезагрузка)', () => {
-      const main = cy.get('[data-cy="main"]');
+      const main = cy.get(mainSelector);
       main.click();
       cy.reload(true);
-      cy.get('#modals').children().should('have.length', 2);
+      cy.get(modalsSelector).children().should('have.length', 2);
     });
 
     it('проверка модального окна (закрытие)', () => {
-      cy.get('[data-cy="main"]').click();
-      cy.get('#modals button:first-of-type').click();
+      cy.get(mainSelector).click();
+      cy.get(modalsSelector + ' button:first-of-type').click();
       cy.wait(200);
-      cy.get('#modals').children().should('have.length', 0);
+      cy.get(modalsSelector).children().should('have.length', 0);
     });
 
     it('проверка модального окна (закрытие на оверлей)', () => {
-      cy.get('[data-cy="main"]').click();
-      cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+      cy.get(mainSelector).click();
+      cy.get(modalsSelector + '>div:nth-of-type(2)').click({ force: true });
       cy.wait(200);
-      cy.get('#modals').children().should('have.length', 0);
+      cy.get(modalsSelector).children().should('have.length', 0);
     });
 
     it('проверка модального окна (закрытие esc)', () => {
-      cy.get('[data-cy="main"]').click();
+      cy.get(mainSelector).click();
       cy.get('body').type('{esc}');
       cy.wait(200);
-      cy.get('#modals').children().should('have.length', 0);
+      cy.get(modalsSelector).children().should('have.length', 0);
     });
   });
 });
@@ -67,33 +74,33 @@ describe('проверяем оформление заказа', () => {
     cy.intercept('GET', 'api/auth/user', { fixture: 'user' });
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
     cy.intercept('POST', 'api/orders', { fixture: 'order' });
-    cy.visit('http://localhost:4000');
+    cy.visit(testUrl);
   });
 
   it('проверка на пустой заказ', () => {
-    cy.get('[data-cy="order-button"]').click();
-    cy.get('#modals').children().should('have.length', 0);
+    cy.get(orderButtonSelector).click();
+    cy.get(modalsSelector).children().should('have.length', 0);
   });
 
   it('проверка на оформление заказа', () => {
-    cy.get('[data-cy="bun"]').find('.common_button').click();
-    cy.get('[data-cy="main"]').find('.common_button').click();
-    cy.get('[data-cy="sauce"]').find('.common_button').click();
+    cy.get(bunSelector).find('.common_button').click();
+    cy.get(mainSelector).find('.common_button').click();
+    cy.get(sauceSelector).find('.common_button').click();
 
-    cy.get('[data-cy="order-button"]').click();
-    cy.get('#modals').children().should('have.length', 2);
+    cy.get(orderButtonSelector).click();
+    cy.get(modalsSelector).children().should('have.length', 2);
 
-    cy.get('#modals h2:first-of-type').should(
+    cy.get(modalsSelector + ' h2:first-of-type').should(
       'have.text',
       orderJson.order.number
     );
 
     cy.get('body').type('{esc}');
     cy.wait(200);
-    cy.get('#modals').children().should('have.length', 0);
+    cy.get(modalsSelector).children().should('have.length', 0);
 
-    cy.get('[data-cy="order-button"]').click();
-    cy.get('#modals').children().should('have.length', 0);
+    cy.get(orderButtonSelector).click();
+    cy.get(modalsSelector).children().should('have.length', 0);
   });
 
   afterEach(() => {
